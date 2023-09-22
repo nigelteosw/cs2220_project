ca2cac2f95c75269156b76eb1716294491176af6
Nigel Teo <nigelteosw@gmail.com>
2023-09-22 16:14:22 +0800

    Update page.js

---
+++ app/src/pages/results.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Results() {
+  // You can fetch and display the results data here, or use props if needed
+
+  return (
+    <div className="container">
+      <h1 className="text-4xl font-bold mb-4">Results Page</h1>
+      <p>This is the results page for your protein data.</p>
+      {/* Display your results data here */}
+    </div>
+  );
+}
