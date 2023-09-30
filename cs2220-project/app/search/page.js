f9f3bb4789fe4cf43fba1ffb0ef68e71a6988c3b
Nigel Teo <nigelteosw@gmail.com>
2023-10-01 00:52:27 +0800

    Update searchform.js

--- cs2220-project/app/search/page.js
+++ cs2220-project/app/search/page.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import Navbar from '../../components/navbar.js';
-import SearchForm from '@/components/searchform.js';
+import React from "react";
+import Navbar from "../../components/navbar.js";
+import SearchForm from "@/components/searchform.js";
+import { Card } from "@mui/material";
 
 function SearchPage() {
-    return (
-        <main>
-            <Navbar />
-            <SearchForm />
-        </main>
-    );
-  }
-  
-  export default SearchPage;
+	return (
+		<main>
+			<Navbar />
+			<Card
+				sx={{
+					minWidth: 275,
+					padding: 6, // Add padding to the Card
+					marginTop: 4, // Add margin from the top
+                    marginLeft: 20, // Add margin from the left
+                    marginRight: 20, // Add margin from the right
+				}}
+				variant="outlined"
+			>
+				<SearchForm />
+			</Card>
+		</main>
+	);
+}
+
+export default SearchPage;
--- cs2220-project/components/searchform.js
+++ cs2220-project/components/searchform.js
@@ -1,111 +1,62 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+"use client";
+
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
 
 const SearchForm = () => {
-  return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Shipping address
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="firstName"
-            name="firstName"
-            label="First name"
-            fullWidth
-            autoComplete="given-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastName"
-            name="lastName"
-            label="Last name"
-            fullWidth
-            autoComplete="family-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="address1"
-            name="address1"
-            label="Address line 1"
-            fullWidth
-            autoComplete="shipping address-line1"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="address2"
-            name="address2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="shipping address-line2"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="shipping postal-code"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
-          />
-        </Grid>
-      </Grid>
-    </React.Fragment>
-  );
-}
+	const handleSearch = () => {
+		// Handle the search logic here
+        // You can access the input values from the form fields and perform a search
+        // For now, let's just log the values to the console
+        console.log("Protein Name:", document.getElementById("proteinName").value);
+        console.log("Pubmed ID:", document.getElementById("pubmedID").value);
+    };
+
+
+	return (
+		<React.Fragment>
+			<Typography variant="h4" gutterBottom>
+				Search Proteins
+			</Typography>
+			<Grid container spacing={3}>
+				<Grid item xs={12} sm={6}>
+					<TextField
+						required
+						id="proteinName"
+						name="proteinName"
+						label="Protein name"
+						fullWidth
+						autoComplete="given-name"
+						variant="standard"
+					/>
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<TextField
+						id="pubmedID"
+						name="pubmedID"
+						label="Pubmed ID"
+						fullWidth
+						autoComplete="given-name"
+						variant="standard"
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<Button
+						variant="contained"
+						color="primary"
+						onClick={handleSearch}
+					>
+						Search
+					</Button>
+				</Grid>
+			</Grid>
+		</React.Fragment>
+	);
+};
 
 export default SearchForm;
