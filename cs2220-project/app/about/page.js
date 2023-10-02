e58027d33e3a41f3316cfe214980c00f0aec20a3
Nigel Teo <nigelteosw@gmail.com>
2023-10-03 02:56:32 +0800

    Update page.js

--- cs2220-project/app/about/page.js
+++ cs2220-project/app/about/page.js
@@ -1,35 +1,42 @@
-import React from 'react';
-import Navbar from '../../components/navbar.js';
+import React from "react";
+import Navbar from "../../components/navbar.js";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 
 function AboutPage() {
-  return (
-    <main>
-      <Navbar />
-      <Container maxWidth="lg" style={{ marginTop: '20px' }}>
-        <Paper elevation={3} style={{ padding: '20px', marginBottom: '20px' }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            About the Project
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Welcome to our project's About page. We're excited to share
-            information about our project with you.
-          </Typography>
-          <Typography variant="body1" paragraph>
-            <strong>Purpose:</strong> Our project's purpose is to develop a database for interaction energy between two proteins.
-          </Typography>
-          <Typography variant="body1" paragraph>
-            <strong>Technologies Used:</strong> We use a variety of
-            technologies to power our project, including React, Next.js, and
-            Material-UI for the frontend. On the backend, we utilize Next.js. Our database is powered by Planetscale.
-          </Typography>
-          {/* Add more information about your project */}
-        </Paper>
-      </Container>
-    </main>
-  );
+	return (
+		<main>
+			<Navbar />
+			<Container maxWidth="lg" style={{ marginTop: "20px" }}>
+				<Paper
+					elevation={3}
+					style={{ padding: "20px", marginBottom: "20px" }}
+				>
+					<Typography variant="h4" component="h1" gutterBottom>
+						About the Project
+					</Typography>
+					<Typography variant="body1" paragraph>
+						Welcome to our projects About page. We are excited to
+						share information about our project with you.
+					</Typography>
+					<Typography variant="body1" paragraph>
+						<strong>Purpose:</strong> Our projects purpose is to
+						develop a database for interaction energy between two
+						proteins.
+					</Typography>
+					<Typography variant="body1" paragraph>
+						<strong>Technologies Used:</strong> We use a variety of
+						technologies to power our project, including React,
+						Next.js, and Material-UI for the frontend. On the
+						backend, we utilize Next.js. Our database is powered by
+						Planetscale.
+					</Typography>
+					{/* Add more information about your project */}
+				</Paper>
+			</Container>
+		</main>
+	);
 }
 
 export default AboutPage;
