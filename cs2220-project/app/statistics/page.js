b24c7e657bf0035377735eca6916b4a24af2773c
Nigel Teo <nigelteosw@gmail.com>
2023-10-03 19:10:53 +0800

    Update statistics/page.js

--- cs2220-project/app/statistics/page.js
+++ cs2220-project/app/statistics/page.js
@@ -1,20 +1,26 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Navbar from "../../components/navbar.js";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import { useEffect, useRef } from "react";
-import Chart from "chart.js/auto";
 import { Grid } from "@mui/material";
+import Chart from "chart.js/auto";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
 
 const StatisticsPage = () => {
-	const chartRef = useRef(null);
-	let myChart = null; // To keep track of the chart instance
+	const chartRefExperiment = useRef(null);
+	const chartRefJournal = useRef(null);
+	let myChartExperiment = null; // To keep track of the experiment chart instance
+	let myChartJournal = null; // To keep track of the journal chart instance
 
-	// Actual data for pie chart
-	const pieChartData = {
+	// Actual data for pie charts
+	const pieChartExperimentData = {
 		labels: [
 			"Competition ELISA",
 			"Competitive Binding",
@@ -30,32 +36,86 @@ const StatisticsPage = () => {
 			{
 				data: [20, 21, 130, 4, 3, 4, 9, 14, 335], // Replace these values with your actual counts
 				backgroundColor: [
-					"#FF5733",
-					"#335BFF",
-					"#FFD433",
-					"#32a852",
-					"#ab32a8",
-					"#32a8a1",
-					"#a83232",
-					"#3297a8",
-					"#ffa832",
-				], // You can define custom colors here
+					"#F44336",
+					"#E91E63",
+					"#9C27B0",
+					"#673AB7",
+					"#3F51B5",
+					"#2196F3",
+					"#03A9F4",
+					"#00BCD4",
+					"#009688",
+				],
 			},
 		],
 	};
 
-	useEffect(() => {
-		if (myChart) {
-			// If the chart instance exists, destroy it before creating a new one
-			myChart.destroy();
-		}
+	const pieChartJournalData = {
+		labels: [
+			"Biochemistry",
+			"EMBO J.",
+			"Eur. J. Biochem.",
+			"Immunity",
+			"J. Biol. Chem.",
+			"J. Exp. Med.",
+			"J. Mol. Biol.",
+			"Mol. Cell",
+			"Nat. Struct. Mol. Biol.",
+			"Proc. Natl. Acad. Sci. U.S.A.",
+			"Science",
+		],
+		datasets: [
+			{
+				data: [56, 18, 17, 17, 67, 14, 149, 4, 7, 121, 21], // Replace these values with your actual counts
+				backgroundColor: [
+					"#F44336",
+					"#E91E63",
+					"#9C27B0",
+					"#673AB7",
+					"#3F51B5",
+					"#2196F3",
+					"#03A9F4",
+					"#00BCD4",
+					"#009688",
+					"#4CAF50",
+					"#FFC107",
+				],
+			},
+		],
+	};
 
-		// Create a pie chart
-		const ctx = chartRef.current.getContext("2d");
-		myChart = new Chart(ctx, {
+	const tableData = [
+		{ label: "Number of Entries", value: 540 },
+		{ label: "Average Binding free energy (ΔG)", value: -11.50259 },
+		{ label: "Average PH", value: 7.3867 },
+		{ label: "Average of Temperature", value: 298 },
+		{ label: "PDB IDs", value: 25 },
+	];
+
+	useEffect(() => {
+		// Create the experiment pie chart
+		const ctxExperiment = chartRefExperiment.current.getContext("2d");
+		myChartExperiment = new Chart(ctxExperiment, {
 			type: "pie",
-			data: pieChartData,
+			data: pieChartExperimentData,
 		});
+
+		// Create the journal pie chart
+		const ctxJournal = chartRefJournal.current.getContext("2d");
+		myChartJournal = new Chart(ctxJournal, {
+			type: "pie",
+			data: pieChartJournalData,
+		});
+
+		return () => {
+			// Cleanup: Destroy the chart instances when the component unmounts
+			if (myChartExperiment) {
+				myChartExperiment.destroy();
+			}
+			if (myChartJournal) {
+				myChartJournal.destroy();
+			}
+		};
 	}, []);
 
 	return (
@@ -82,16 +142,76 @@ const StatisticsPage = () => {
 							sm={6}
 							container
 							justifyContent="flex-end"
-						></Grid>
+						>
+							{/* Add any additional content here */}
+						</Grid>
 					</Grid>
 				</Paper>
 				<Grid container spacing={2}>
-					<Grid item xs={12}>
+					<Grid item xs={12} sm={4} padding={3}>
 						<Paper
 							elevation={3}
-							style={{ height: 600, width: "100%" }}
+							style={{ height: "100%", width: "100%" }}
 						>
-							<canvas ref={chartRef} />
+							<Typography
+								variant="h6"
+								component="h2"
+								paddingTop={2}
+								paddingLeft={2}
+							>
+								Summary Statistics
+							</Typography>
+							<TableContainer component={Paper}>
+								<Table aria-label="Statistics Table">
+									<TableBody>
+										{tableData.map((row) => (
+											<TableRow key={row.label}>
+												<TableCell
+													component="th"
+													scope="row"
+												>
+													{row.label}
+												</TableCell>
+												<TableCell align="right">
+													{row.value}
+												</TableCell>
+											</TableRow>
+										))}
+									</TableBody>
+								</Table>
+							</TableContainer>
+						</Paper>
+					</Grid>
+					<Grid item xs={12} sm={4} padding={3}>
+						<Paper
+							elevation={3}
+							style={{ height: "100%", width: "100%" }}
+						>
+							<Typography
+								variant="h6"
+								component="h2"
+								paddingTop={2}
+								paddingLeft={2}
+							>
+								Types of Experiments
+							</Typography>
+							<canvas ref={chartRefExperiment} />
+						</Paper>
+					</Grid>
+					<Grid item xs={12} sm={4} padding={3}>
+						<Paper
+							elevation={3}
+							style={{ height: "100%", width: "100%" }}
+						>
+							<Typography
+								variant="h6"
+								component="h2"
+								paddingTop={2}
+								paddingLeft={2}
+							>
+								Journals
+							</Typography>
+							<canvas ref={chartRefJournal} />
 						</Paper>
 					</Grid>
 				</Grid>
